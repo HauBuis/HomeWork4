76aab6fb3f7470ace3889a65dd5cc2a357907fe2
CongHau <conghau0704900193@example.com>
2025-06-23 21:41:47 +0700

    update

--- app/src/main/java/com/example/bai4/MainActivity.java
+++ app/src/main/java/com/example/bai4/MainActivity.java
@@ -1,46 +1,48 @@
 package com.example.bai4;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main); // ✅ sửa lại đúng layout
+        setContentView(R.layout.activity_main);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.gridLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button btnCountries = findViewById(R.id.btnCountries);
-        Button btnLeaders = findViewById(R.id.btnLeaders);
-        Button btnMuseums = findViewById(R.id.btnMuseums);
-        Button btnWonders = findViewById(R.id.btnWonders);
 
-        btnCountries.setOnClickListener(v ->
-                startActivity(new Intent(MainActivity.this, com.example.bai4.CountriesActivity.class)));
+        CardView cardCountries = findViewById(R.id.cardCountries);
+        CardView cardLeaders = findViewById(R.id.cardLeaders);
+        CardView cardMuseums = findViewById(R.id.cardMuseums);
+        CardView cardWonders = findViewById(R.id.cardWonders);
 
-        // Các activity còn lại bạn sẽ tạo tương tự
-        btnLeaders.setOnClickListener(v ->
+
+        cardCountries.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, CountriesActivity.class)));
+
+        cardLeaders.setOnClickListener(v ->
                 startActivity(new Intent(MainActivity.this, LeadersActivity.class)));
 
-        btnMuseums.setOnClickListener(v ->
+        cardMuseums.setOnClickListener(v ->
                 startActivity(new Intent(MainActivity.this, MuseumsActivity.class)));
 
-        btnWonders.setOnClickListener(v ->
+        cardWonders.setOnClickListener(v ->
                 startActivity(new Intent(MainActivity.this, WondersActivity.class)));
     }
 }
